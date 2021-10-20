3a2cdc7d05fea01b7a893c1a0332c8e9c28f70aa
Nicola Toscan <nicolatoscan99@outlook.it>
2021-10-20 16:23:07 +0200

    booking

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import * as dotenv from 'dotenv'
 import { Telegraf, Context } from "telegraf";
-import { Update } from 'typegram';
-import * as scrapper from './scrapper';
 import Session from './session';
+import Scrapper from './scrapper';
 dotenv.config()
 
 
 class LaundryBot {
     private bot: Telegraf<Context>;
     private session: Session;
+    private scrapper: Scrapper;
 
     constructor() {
         const TOKEN = process.env.BOT_TOKEN
@@ -18,11 +18,13 @@ class LaundryBot {
         }
 
         this.session = new Session();
+        this.scrapper = new Scrapper();
 
         this.bot = new Telegraf(TOKEN);
         this.bot.start((ctx: Context) => ctx.reply('Welcome!'));
         this.bot.command('username', (ctx) => this.addUsername(ctx, ctx.from.id, ctx.message.text));
         this.bot.command('available', (ctx) => this.getAvailableSlots(ctx, ctx.from.id));
+        this.bot.command('book', (ctx) => this.book(ctx, ctx.from.id, ctx.message.text));
 
         this.bot.launch();
         console.log('Bot started');
@@ -30,7 +32,7 @@ class LaundryBot {
 
     private addUsername(ctx: Context, userId: number, text: string) {
         const username = text.split(' ')[1];
-        if (username.length != 13) {
+        if (!username || username.length != 13) {
             ctx.reply('Username must be in te format:\n/username 0000-0000-000')
         }
         this.session.addUsername(userId, username);
@@ -43,9 +45,23 @@ class LaundryBot {
             ctx.reply('You must add a username first!');
             return;
         }
-        const msg = await ctx.reply('Loading ...');
-        const res = await scrapper.getAvailableSlots(username);
-        ctx.telegram.editMessageText(ctx.chat?.id, msg.message_id, undefined, res.join('\n'));
+        const msgPromise = ctx.reply('Loading ...');
+        const res = await this.scrapper.getAvailableSlots(username);
+        ctx.telegram.editMessageText(ctx.chat?.id, (await msgPromise).message_id, undefined, res.join('\n'));
+    }
+
+    private async book(ctx: Context, userId: number, text: string) {
+        const slot = +text.split(' ')[1];
+        if (!slot || slot < 1 || slot > 10) {
+            ctx.reply('Slot must be between 1 and 10');
+            return;
+        }
+
+        const msgPromise = await ctx.reply(`Booking slot ${slot}\nLoading ...`);
+        const username = this.session.getUsername(userId);
+        await this.scrapper.bookSlot(username, slot);
+        await ctx.telegram.deleteMessage(ctx.chat!.id, (await msgPromise).message_id);
+        await ctx.replyWithPhoto({ source: `./assets/${username}.png` }, { caption: `Slot ${slot} Booked!` });
     }
 
 }
--- src/scrapper.ts
+++ src/scrapper.ts
@@ -1,29 +1,58 @@
 import puppeteer from 'puppeteer';
 import { parse, HTMLElement } from 'node-html-parser';
 
-export async function getAvailableSlots(username: string): Promise<string[]> {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://sssb.aptustotal.se/AptusPortal/Account/Login');
-    await page.type('#UserName', username);
-    await page.type('#Password', username);
-    await page.click('#btnLogin');
-    await page.waitForNavigation({waitUntil: 'networkidle2'})
-    await page.goto('https://sssb.aptustotal.se/AptusPortal/CustomerBooking/FirstAvailable?categoryId=35&firstX=10')
-    const html = await page.$eval('#content', el => el.innerHTML)
-    const parsedHtml = parse(html);
-    const cards = parsedHtml.querySelectorAll('.bookingCard');
-    const avaibleSlots = cards.map(c => {
-        const parseText = (text: HTMLElement) => {
-            return text.text.trim().replace(/\n/g, '');
+export default class Scrapper {
+
+    private headless: boolean;
+    private browser: puppeteer.Browser | null = null;
+
+    constructor(headless: boolean = false) {
+        this.headless = headless;
+    }
+
+    private async getBrowser(): Promise<puppeteer.Browser> {
+        if (this.browser === null) {
+            this.browser = await puppeteer.launch({ headless: this.headless });
         }
-        const divs = c.querySelectorAll('div');
-        const time = parseText(divs[0]);
-        const date = parseText(divs[1]);
-        const gruppo = parseText(divs[3]);
-        const edificio = parseText(divs[4]);
-        return `${time} ${date}\n${gruppo} | ${edificio}\n`;
-    })
-    await browser.close();
-    return avaibleSlots;
+        return this.browser;
+    }
+
+    private async loadAvailableSlotsPage(username: string): Promise<puppeteer.Page> {
+        const page = await (await this.getBrowser()).newPage();
+        await page.goto('https://sssb.aptustotal.se/AptusPortal/Account/Login');
+        await page.type('#UserName', username);
+        await page.type('#Password', username);
+        await page.click('#btnLogin');
+        await new Promise(r => setTimeout(r, 500));
+        await page.goto('https://sssb.aptustotal.se/AptusPortal/CustomerBooking/FirstAvailable?categoryId=35&firstX=10')
+        return page;
+    }
+
+    public async getAvailableSlots(username: string): Promise<string[]> {
+        const page = await this.loadAvailableSlotsPage(username);
+        const html = await page.$eval('#content', el => el.innerHTML)
+        const parsedHtml = parse(html);
+        const cards = parsedHtml.querySelectorAll('.bookingCard');
+        const avaibleSlots = cards.map((c, i) => {
+            const parseText = (text: HTMLElement) => {
+                return text.text.trim().replace(/\n/g, '');
+            }
+            const divs = c.querySelectorAll('div');
+            const time = parseText(divs[0]);
+            const date = parseText(divs[1]);
+            const gruppo = parseText(divs[3]);
+            const edificio = parseText(divs[4]);
+            return `${i+1}) ${time} ${date}\n${gruppo} | ${edificio}\n`;
+        })
+        page.close()
+        return avaibleSlots;
+    }
+
+    public async bookSlot(username: string, slot: number): Promise<void> {
+        const page = await this.loadAvailableSlotsPage(username);
+        await page.click(`.bookingCard:nth-child(${slot + 1}) button`);
+        await new Promise(r => setTimeout(r, 1000));
+        await page.screenshot({path: `./assets/${username}.png`});
+        await page.close();
+    }
 }
